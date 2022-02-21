028b9699e6080f2856a5910a8289e51671b83a5d
Ashok Sharma <ashok.sharma@slscorp.com>
2022-02-21 19:55:36 +0530

    created simple test file and used toolchain wihtout ide to compile

---
+++ main.s
@@ -0,0 +1,41 @@
+	.cpu cortex-m0
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"main.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello World\000"
+	.text
+	.align	1
+	.global	main
+	.arch armv6s-m
+	.syntax unified
+	.code	16
+	.thumb_func
+	.fpu softvfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{r7, lr}
+	add	r7, sp, #0
+	ldr	r3, .L3
+	movs	r0, r3
+	bl	puts
+.L2:
+	b	.L2
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (GNU Arm Embedded Toolchain 10.3-2021.07) 10.3.1 20210621 (release)"
